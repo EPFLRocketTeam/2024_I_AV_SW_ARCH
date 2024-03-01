eecf821ff503e80b6d4f14dd790c0ff9532de9c4
BuildTools <unconfigured@null.spigotmc.org>
2024-03-01 13:25:50 +0100

    circular buffer

--- include/common/core/channel/circular_buffer.hpp
+++ include/common/core/channel/circular_buffer.hpp
@@ -5,11 +5,38 @@
 #ifndef INC_2024_I_AV_SW_CIRCULAR_BUFFER_HPP
 #define INC_2024_I_AV_SW_CIRCULAR_BUFFER_HPP
 
+template<size_t MAX_SIZE>
 class CircularBuffer {
+private:
+	uint8_t buffer[MAX_SIZE];
+	uint32_t head = 0;
+	uint32_t tail = 0;
+	uint32_t size = 0;
+
 public:
 	void pushBack(const uint8_t& byte);
 	uint8_t popFront();
 	bool isDataAvailable();
 };
+
+template<size_t MAX_SIZE>
+void CircularBuffer<MAX_SIZE>::pushBack(const uint8_t &byte) {
+	buffer[tail] = byte;
+	tail = (tail + 1) % MAX_SIZE;
+	size++;
+}
+
+template<size_t MAX_SIZE>
+uint8_t CircularBuffer<MAX_SIZE>::popFront() {
+	uint8_t byte = buffer[head];
+	head = (head + 1) % MAX_SIZE;
+	size--;
+	return byte;
+}
+
+template<size_t MAX_SIZE>
+bool CircularBuffer<MAX_SIZE>::isDataAvailable() {
+	return size > 0;
+}
 
 #endif //INC_2024_I_AV_SW_CIRCULAR_BUFFER_HPP
